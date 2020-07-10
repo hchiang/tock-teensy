37536bb56bca1ae1eb501fcfb03ab7f2f050b5ea
Holly Chiang <hchiang1@stanford.edu>
2020-07-09 21:45:46 -0700

    basic no clock manager code

--- apps/examples/flash-adc/main.c
+++ apps/examples/flash-adc/main.c
@@ -20,7 +20,7 @@ float movingAvg(float prev_avg, int num_samples, int new_val)
 
 int main(void) {
 
-  int adc_length = 600;
+  int adc_length = 500;
   uint16_t adc_buffer[adc_length];
   uint8_t channel = 0;
   uint32_t freq = 125000;
@@ -36,23 +36,27 @@ int main(void) {
   ret = nonvolatile_storage_internal_write_done_subscribe(write_done, NULL);
   if (ret != 0) printf("ERROR setting write done callback\n");
 
-  printf("Begin\n");
+  //printf("Begin\n");
   while(true) {
-    int err = adc_sample_buffer_sync(channel, freq, adc_buffer, adc_length);
-    if (err < 0) {
-        printf("Error sampling ADC: %d\n", err);
+    int k, i, j;
+    for(i=0; i<4; i++) {
+        int err = adc_sample_buffer_sync(channel, freq, adc_buffer, adc_length);
+        if (err < 0) {
+            printf("Error sampling ADC: %d\n", err);
+        }
     }
 
-    int k, i;
-    for (k=0; k<adc_length/16; k++) {
-      for (i=k*16; i<(k+1)*16; i++) {
-        fft_buf[i % 16] = adc_buffer[i]; // Copy needed bc fft alg I found is int not uint16
-      }
-      fft(fft_buf, fft_mag);
-      int l;
-      // For each returned fft magnitude, update the moving average for that magnitude bin
-      for (l=3; l<8; l++) {
-        avg_fft_mag[l] = movingAvg(avg_fft_mag[l], k, fft_mag[l]);
+    for(j=0; j<4; i++) {
+      for (k=0; k<adc_length/16; k++) {
+        for (i=k*16; i<(k+1)*16; i++) {
+          fft_buf[i % 16] = adc_buffer[i]; // Copy needed bc fft alg I found is int not uint16
+        }
+        fft(fft_buf, fft_mag);
+        int l;
+        // For each returned fft magnitude, update the moving average for that magnitude bin
+        for (l=3; l<8; l++) {
+          avg_fft_mag[l] = movingAvg(avg_fft_mag[l], k, fft_mag[l]);
+        }
       }
     }
 
@@ -66,7 +70,7 @@ int main(void) {
     }
     yield_for(&wdone);
 
-    printf("Done\n");
+    //printf("Done\n");
 
     delay_ms(500);
   }
