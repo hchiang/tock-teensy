699a60ba4e9e48a70044ee96af08075a990bd8f0
Holly Chiang <hchiang1@stanford.edu>
2020-07-10 20:05:04 -0700

    updated app

--- apps/examples/flash-adc/main.c
+++ apps/examples/flash-adc/main.c
@@ -18,12 +18,13 @@ float movingAvg(float prev_avg, int num_samples, int new_val)
   return prev_avg + ((float)(new_val) - prev_avg) / num_samples;
 }
 
+#define ADC_SAMPLES 4000
+uint16_t adc_buffer[ADC_SAMPLES];
+
 int main(void) {
 
-  int adc_length = 500;
-  uint16_t adc_buffer[adc_length];
   uint8_t channel = 0;
-  uint32_t freq = 500000;
+  uint32_t freq = 130000;
 
   int fft_buf[16];
   int fft_mag[8];
@@ -38,40 +39,36 @@ int main(void) {
 
   //printf("Begin\n");
   while(true) {
-    int i, j, k;
+    int i, k;
     //Stack issues if we increase adc buffer size so instead sample multiple times
-    for(i=0; i< 4; i++) {
-        int err = adc_sample_buffer_sync(channel, freq, adc_buffer, adc_length);
-        if (err < 0) {
-            printf("Error sampling ADC: %d\n", err);
-        }
+    int err = adc_sample_buffer_sync(channel, freq, adc_buffer, ADC_SAMPLES);
+    if (err < 0) {
+        printf("Error sampling ADC: %d\n", err);
     }
-
-    for(j=0; j<4; j++) {
-        for (k=0; k<adc_length/16; k++) {
-          for (i=k*16; i<(k+1)*16; i++) {
-            fft_buf[i % 16] = adc_buffer[i]; // Copy needed bc fft alg I found is int not uint16
-          }
-          fft(fft_buf, fft_mag);
-          int l;
-          // For each returned fft magnitude, update the moving average for that magnitude bin
-          for (l=3; l<8; l++) {
-            avg_fft_mag[l] = movingAvg(avg_fft_mag[l], k, fft_mag[l]);
-          }
-        }
+    
+    for (k=0; k<ADC_SAMPLES/16; k++) {
+      for (i=k*16; i<(k+1)*16; i++) {
+        fft_buf[i % 16] = adc_buffer[i]; // Copy needed bc fft alg I found is int not uint16
+      }
+      fft(fft_buf, fft_mag);
+      int l;
+      // For each returned fft magnitude, update the moving average for that magnitude bin
+      for (l=3; l<8; l++) {
+        avg_fft_mag[l] = movingAvg(avg_fft_mag[l], k, fft_mag[l]);
+      }
     }
-
+    
     memcpy(writebuf, (void*)&avg_fft_mag[0], flash_len);
 
     wdone = false;
-    ret  = nonvolatile_storage_internal_write(0, 2000);
+    ret  = nonvolatile_storage_internal_write(0, flash_len);
     if (ret != 0) {
       printf("\tERROR calling write\n");
       return ret;
     }
     yield_for(&wdone);
 
-   //printf("Done\n");
+    //printf("Done\n");
 
     delay_ms(500);
   }
